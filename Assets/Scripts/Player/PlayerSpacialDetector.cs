23097b0170aea4280263e1b1144516483243f914
SebasRodJos <132218836+SebasRodJos@users.noreply.github.com>
2025-01-28 21:58:50 -0300

    Funciona Forro

--- Assets/Scripts/Player/PlayerSpacialDetector.cs
+++ Assets/Scripts/Player/PlayerSpacialDetector.cs
@@ -20,19 +20,4 @@ public class PlayerSpacialDetector : MonoBehaviour
     {
         return Physics2D.Raycast(transform.position, Vector2.down, 100f, detectionLayerMask).point.y + (actorHeight / 2f);
     }
-
-    public Enemy DetectEnemy(float direction, float lookAhead)
-    {
-        Vector2 rayOrigin = new Vector2(transform.position.x, transform.position.y - (actorHeight / 2f));
-        Vector2 rayDirection = Vector2.right * direction;
-
-        RaycastHit2D hit = Physics2D.Raycast(rayOrigin, rayDirection, actorWidth / 2f + lookAhead, detectionLayerMask);
-        Debug.DrawRay(rayOrigin, (rayDirection * lookAhead), Color.blue, 1);
-
-        if (hit.collider != null)
-        {
-            return hit.collider.GetComponent<Enemy>();
-        }
-        else { return null; }
-    }
 }
