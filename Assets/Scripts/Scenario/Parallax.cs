4b8ee88c7a5aacea4ce5661f9cfe128444647f11
JP206 <juanpecose@hotmail.com>
2025-01-29 21:29:52 -0300

    scenario pool

--- Assets/Scripts/Scenario/Parallax.cs
+++ Assets/Scripts/Scenario/Parallax.cs
@@ -3,7 +3,12 @@ using UnityEngine;
 public class Parallax : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] bool mountain;
+
+    ScenarioPool pool;
     SpriteRenderer spriteRenderer;
+    bool spawnFlag = false;
+    
     float camMaxX => Camera.main.ViewportToWorldPoint(new Vector3(1.0f, 0f, 0f)).x;
     float camMinX => Camera.main.ViewportToWorldPoint(new Vector3(0f, 0f, 0f)).x;
 
@@ -12,7 +17,6 @@ public class Parallax : MonoBehaviour
         spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
-
     void Update()
     {
         transform.Translate(Time.deltaTime * speed * Vector2.left);
@@ -20,12 +24,33 @@ public class Parallax : MonoBehaviour
         if (spriteRenderer.bounds.max.x <= camMaxX)
         {
             Vector3 spawnPoint = new Vector3(transform.position.x + spriteRenderer.bounds.extents.x * 2, transform.position.y, 0);
-            Instantiate(gameObject, spawnPoint, Quaternion.identity);
+            if (mountain && !spawnFlag)
+            {
+                spawnFlag = true;
+                GameObject mountainInstance = pool.GetPooledMountain();
+                mountainInstance.transform.position = spawnPoint;
+            }
+            else if (!spawnFlag)
+            {
+                spawnFlag = true;
+                GameObject mountainInstance = pool.GetPooledRock();
+                mountainInstance.transform.position = spawnPoint;
+            }
         }
 
         if (spriteRenderer.bounds.max.x <= camMinX)
         {
-            Destroy(gameObject);
+            gameObject.SetActive(false);
         }
+    }
+
+    public void SetPool(ScenarioPool _pool)
+    {
+        pool = _pool;
+    }
+
+    private void OnDisable()
+    {
+        spawnFlag = false;
     }
 }
--- Assets/Scripts/Scenario/ScenarioPool.cs
+++ Assets/Scripts/Scenario/ScenarioPool.cs
@@ -1,16 +1,83 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class ScenarioPool : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    List<GameObject> pooledMountains, pooledRocks;
+
+    [Header("Prefabs")]
+    [SerializeField] GameObject mountainPrefab; 
+    [SerializeField] GameObject rockPrefab;
+
+    [Header("Pool configuration")]
+    [SerializeField] int mountainPoolSize;
+    [SerializeField] int rockPoolSize;
+
+    [Header("Objects starting position")]
+    [SerializeField] float startPosXMountain; 
+    [SerializeField] float startPosYMountain, startPosXRock, startPosYRock;
+
     void Start()
     {
-        
+        InitializeMountains();
+        InitializeRocks();
     }
 
-    // Update is called once per frame
-    void Update()
+    void InitializeMountains()
     {
-        
+        pooledMountains = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < mountainPoolSize; i++)
+        {
+            tmp = Instantiate(mountainPrefab);
+            tmp.GetComponent<Parallax>().SetPool(this);
+            tmp.SetActive(false);
+            pooledMountains.Add(tmp);
+        }
+
+        pooledMountains[0].SetActive(true);
+        pooledMountains[0].transform.position = new Vector3(startPosXMountain, startPosYMountain, 0);
+    }
+
+    void InitializeRocks()
+    {
+        pooledRocks = new List<GameObject>();
+        GameObject tmp;
+        for (int i = 0; i < rockPoolSize; i++)
+        {
+            tmp = Instantiate(rockPrefab);
+            tmp.GetComponent<Parallax>().SetPool(this);
+            tmp.SetActive(false);
+            pooledRocks.Add(tmp);
+        }
+
+        pooledRocks[0].SetActive(true);
+        pooledRocks[0].transform.position = new Vector3(startPosXRock, startPosYRock, 0);
+    }
+
+    public GameObject GetPooledMountain()
+    {
+        for (int i = 0; i < pooledMountains.Count; i++)
+        {
+            if (!pooledMountains[i].activeInHierarchy)
+            {
+                pooledMountains[i].SetActive(true);
+                return pooledMountains[i];
+            }
+        }
+        return null;
+    }
+
+    public GameObject GetPooledRock()
+    {
+        for (int i = 0; i < pooledRocks.Count; i++)
+        {
+            if (!pooledRocks[i].activeInHierarchy)
+            {
+                pooledRocks[i].SetActive(true);
+                return pooledRocks[i];
+            }
+        }
+        return null;
     }
 }
