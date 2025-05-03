2e4331ea5996e220af4c4f463a8116293d33c06a
SebasRodJos <132218836+SebasRodJos@users.noreply.github.com>
2025-05-03 13:52:46 -0300

    Coin Flip

--- Assets/Scripts/Scenario/Miscellaneous/PickerHandler.cs
+++ Assets/Scripts/Scenario/Miscellaneous/PickerHandler.cs
@@ -1,18 +1,37 @@
 using UnityEngine;
+using System.Collections;
 
 public class PickerHandler : MonoBehaviour
 {
+    float duration = 0.6f;
+    float elapsed = 0f;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Dino leg"))
+        if (collision.CompareTag("Dino Leg") || collision.CompareTag("Coin"))
         {
-            Destroy(collision.gameObject);
-        }
-
-        else if (collision.CompareTag("Coin"))
-        {
-            Destroy(collision.gameObject);
+            StartCoroutine(AnimatePickup(collision.gameObject));
         }
     }
+
+    IEnumerator AnimatePickup(GameObject pickup)
+    {
+
+        Vector3 originalPos = pickup.transform.position;
+
+        while (elapsed < duration)
+        {
+            elapsed += Time.deltaTime;
+
+            pickup.transform.Rotate(0f, 1440f * Time.deltaTime, 0f);
+
+            float height = Mathf.Sin((elapsed / duration) * Mathf.PI) * 0.5f;
+            pickup.transform.position = originalPos + new Vector3(0, height, 0);
+
+            yield return null;
+        }
+
+        Destroy(pickup);
+    }
+
 }
